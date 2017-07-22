ef85397e271da290792c6cb342af06ebfa544960
Joshua <ballard.jj@gmail.com>
2017-07-22 09:36:07 -0500

    updated preprocessor to remove duplicate functionality

--- src/driver.py
+++ src/driver.py
@@ -3,16 +3,27 @@ from preprocessor import Preprocessor
 from word_vectors import WordVectors
 
 def main():
-    input_file = "../Default Report.txt"
+    
     preprocessor = Preprocessor()
-    phrases = preprocessor.process_file(input_file)
+    
+    input_file = "../Default Report.txt"
+    separate_lines=True
+    whole_sentences=False
+    stemming=False
+    
+    phrases = preprocessor.process_file(input_file, separate_lines, whole_sentences, stemming)
     rake = Rake()
     keywords = rake.run(phrases)
     print(keywords[:10])
     
-    sentences = preprocessor.parse_input(input_file)    
+    separate_lines=True
+    whole_sentences=True
+    stemming=True
+    threshold = 2
+    
+    phrases = preprocessor.process_file(input_file, separate_lines, whole_sentences, stemming)
     word_vectors = WordVectors()
-    word_vectors.run(sentences, 2)
+    word_vectors.run(phrases, threshold)
 
 
 if __name__ == "__main__":
--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,64 +1,59 @@
 
 from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from gensim.parsing import PorterStemmer
-import re
+#import re
 import string
 
 class Preprocessor(object):
     
     def __init__(self):
-        self.stop_words = stopwords.words('english')        
+        self.stop_words = stopwords.words('english')
+        self.global_stemmer = PorterStemmer()        
             
-    def process_file(self, file):
-        with open(file) as input_file: 
+    def process_file(self, file, separate_lines=False, whole_sentences=True, stemming=False):
+        """Takes input file and parses the text returning a list of list of either phrases or sentences"""
+        with open(file) as input_file:
             text = input_file.read()
-            sentences = self._split_sentences(text)
-            phrases = self._generate_candidate_keywords(sentences)
-        return phrases     
-        
-    def _split_sentences(self, text):        
-        sentence_delimiters = re.compile(u'[.!?,;:\t\\\\"\\(\\)\\\'\u2019\u2013]|\\s\\-\\s')
-        sentences = sentence_delimiters.split(text)
-        return sentences  
-    
-    def _generate_candidate_keywords(self, sentences):
-        phrases = []
-        for sentence in sentences:                     
-            words = map(lambda x: "|" if x in self.stop_words or x in string.punctuation else x, word_tokenize(sentence.lower()))            
-            phrase = []
-            for word in words:                           
-                if word == "|":
-                    if len(phrase) > 0:                        
-                        phrases.append(phrase)
-                        phrase = []
-                else:
-                    phrase.append(word)  
-            if len(phrase) > 0:                
-                phrases.append(phrase)                         
+            phrases = self._parse_text(text, separate_lines, whole_sentences, stemming)                           
         return phrases
     
-    def parse_input(self, filename):
-        """Import the file and parse its contents"""
-        global_stemmer = PorterStemmer()
-        with open(filename) as input_file:
-            stop_words = stopwords.words('english')
+    def process_text(self, text, separate_lines=False, whole_sentences=True, stemming=False):  
+        """Takes input text and parses it returning a list of list of either phrases or sentences"""      
+        phrases = self._parse_text(text, separate_lines, whole_sentences, stemming)                           
+        return phrases
+    
+    def _parse_text(self, text, separate_lines, whole_sentences, stemming):
+        """Parses input text and returns phrases from text"""
+        phrases = []
+        #alpha_pattern = re.compile(r'[^a-zA-Z]')
+        
+        if separate_lines:
+            lines = text.splitlines()
             sentences = []
-            alpha_pattern = re.compile(r'[^a-zA-Z]')
-    
-            for line in input_file:
-                word_list = []
-                words = word_tokenize(line)
-                for word in words:
-                    clean_word = alpha_pattern.sub('', word)
-                    if clean_word:
-                        lower_word = clean_word.lower()
-                        if len(lower_word) > 1 and lower_word not in stop_words:
-                            stemmed = global_stemmer.stem(lower_word)
-                            word_list.append(stemmed)
-    
-                if len(word_list) > 0:
-                    sentences.append(word_list)
-    
-        return sentences
+            for line in lines:
+                sentences += sent_tokenize(line) 
+        else:
+            sentences = sent_tokenize(text)
+         
+        for sentence in sentences:                                      
+            phrase = []
+            words = word_tokenize(sentence.lower())
+            for word in words:                    
+                if word not in self.stop_words and word not in string.punctuation:
+                    #clean_word = alpha_pattern.sub('', word)
+                    #if clean_word:
+                    if stemming:
+                        word = self.global_stemmer.stem(word)
+                    phrase.append(word) 
+                else:
+                    if not whole_sentences: 
+                        if len(phrase) > 0:                        
+                            phrases.append(phrase)                            
+                            phrase = []     
+            if len(phrase) > 0:                
+                phrases.append(phrase)  
+                                       
+        return phrases
     
